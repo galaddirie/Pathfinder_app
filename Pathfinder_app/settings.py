265ed5d772c81a7fd314246bff36f17c7d3fee23
galaddirie <70884733+galaddirie@users.noreply.github.com>
2021-11-13 04:26:30 -0500

    added whitenosie library to serve static files

--- Pathfinder_app/settings.py
+++ Pathfinder_app/settings.py
@@ -47,7 +47,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
 
 ROOT_URLCONF = 'Pathfinder_app.urls'
 
@@ -118,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
