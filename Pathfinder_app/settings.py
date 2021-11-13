01520b26ec28ac8365bd31c1d04c5009611dfcc0
galaddirie <70884733+galaddirie@users.noreply.github.com>
2021-11-13 04:10:16 -0500

    updated settings

--- Pathfinder_app/settings.py
+++ Pathfinder_app/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#9-poq&@go8*5%50xte7$avl^#g2pn+s49t-thgr&-b_ofuft0'
+SECRET_KEY = os.environ.get('PATHFINDER_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('PATHFINDER_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['galadpathfinderapp.herokuapp.com']
 
 
 # Application definition
