72db50f655d0056c2679e6fb07726f64f18f483d
galaddirie <70884733+galaddirie@users.noreply.github.com>
2021-11-13 05:06:08 -0500

    updated allowed host in settings

--- Pathfinder_app/settings.py
+++ Pathfinder_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('PATHFINDER_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('PATHFINDER_DEBUG')
 
-ALLOWED_HOSTS = ['galadpathfinderapp.herokuapp.com']
+ALLOWED_HOSTS = ['galadpathfinderapp.herokuapp.com', '*']
 
 
 # Application definition
